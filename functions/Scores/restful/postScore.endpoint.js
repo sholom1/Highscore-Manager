73c877cc048f80fb3124256f62dfdfe0510d96c5
Sam Kessler <sholomkessler1@gmail.com>
2021-05-06 00:33:51 -0400

    Finished post req

--- functions/Scores/restful/postScore.endpoint.js
+++ functions/Scores/restful/postScore.endpoint.js
@@ -1,19 +1,18 @@
 const { Endpoint, RequestType } = require('firebase-backend');
-const { db } = require('../../config/firebase');
+const { db } = require('../../config/firebase').default;
 
 exports.default = new Endpoint('postScore', RequestType.POST, (req, res) => {
-	const { game, playerName, score } = req.body;
+	const { game, playerName, score } = req.query;
 	try {
 		const entry = db.collection(game).doc();
 		entry.set({
 			id: entry.id,
-			playerName,
-			score,
+			playerName: playerName,
+			score: score,
 		});
 		res.status(201).send({
 			status: 'success',
 			message: 'score added successfully',
-			data: entry,
 		});
 	} catch (error) {
 		res.status(500).json(error.message);
